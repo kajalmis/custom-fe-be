a8040c8f24c66b66b1e52c4336e44ad5a0a63c63
kajalmis <157986496+kajalmis@users.noreply.github.com>
2024-03-21 19:49:41 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -11,7 +11,7 @@ def index():
 @app.route('/api/request', methods=['GET'])
 def send_request():
     # You can add any necessary logic here
-    return '200 OK hello'
+    return '200 OK hello world'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
