69c61629b4bc69740108d8c5bc928db37a55c291
kajalmis <157986496+kajalmis@users.noreply.github.com>
2024-04-27 08:04:21 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ CORS(app)  # This will enable CORS for all routes
 @app.route('/')
 def index():
     return 'Backend: You are viewing  the backend-Kajal.'
-
+ 
 @app.route('/api/request', methods=['GET'])
 def send_request():
     # You can add any necessary logic here
